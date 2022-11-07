d5fdf30ff81f2c2cdaf4adb863ae001dd951910c
bakare <bakaredavid007@gmail.com>
2022-11-07 19:19:58 +0100

    test 1, task 2

--- models/base_model.py
+++ models/base_model.py
@@ -12,6 +12,14 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = datetime.datetime.now()
+        # if len(kwargs) != 0 or kwargs != {}:
+        #     for k, v in  kwargs.items():
+        #         if k == "created_at" or k == "updated_at":
+        #             self.__dict__[k] =
+        #         else:
+        #             self.__dict__[k] = v
+        # else:
+        #     pass
 
     def __str__(self):
         ''' String Magic Method '''
@@ -34,3 +42,26 @@ class BaseModel:
         new_dict['created_at'] = str(self.created_at.isoformat())
         new_dict['updated_at'] = str(self.updated_at.isoformat())
         return new_dict
+
+# my_model = BaseModel()
+# my_model.name = "My_First_Model"
+# my_model.my_number = 89
+# print(my_model.id)
+# print(my_model)
+# print(type(my_model.created_at))
+# print("--")
+# my_model_json = my_model.to_dict()
+# print(my_model_json)
+# print("JSON of my_model:")
+# for key in my_model_json.keys():
+#     print("\t{}: ({}) - {}".format(key, type(my_model_json
+# [key]), my_model_json[key]))
+
+# print("--")
+# my_new_model = BaseModel(**my_model_json)
+# print(my_new_model.id)
+# print(my_new_model)
+# print(type(my_new_model.created_at))
+
+# print("--")
+# print(my_model is my_new_model)
