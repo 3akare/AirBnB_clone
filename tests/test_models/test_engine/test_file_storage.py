db6cd4260d03f54c7d6ab61676565e1b0e3c32ea
bakare <bakaredavid007@gmail.com>
2022-11-07 23:54:32 +0100

    file storage

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+""" File Storage Testing """
+import unittest
+import models
+import json
+from datetime import datetime
+from time import sleep
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+
+
+class TestFileStorage_init(unittest.TestCase):
+    """Unittests for FileStorage init"""
+
+    def test_for_init(self):
+        self.assertEqual(type(FileStorage()), FileStorage)
+
+    def test_presence_of_file_path(self):
+        self.assertIsInstance(FileStorage._FileStorage__file_path, str)
+
+    def test_reload_no_file(self):
+        self.assertRaises(TypeError, models.storage.reload())
+
+    def test_presence_of_objects(self):
+        self.assertIsInstance(FileStorage._FileStorage__objects, dict)
+
+    def test_all(self):
+        self.assertEqual(dict, type(models.storage.all()))
+
+    def test_new(self):
+        bm = BaseModel()
+        models.storage.new(bm)
+        self.assertIn("BaseModel." + bm.id, models.storage.all().keys())
+
+    def test_save(self):
+        bm = BaseModel()
+        models.storage.save()
+        save_text = ""
+        with open("file.json", "r") as f:
+            save_text = f.read()
+            self.assertIn("BaseModel." + bm.id, save_text)
+
+
+if __name__ == '__main__':
+    unittest.main()
