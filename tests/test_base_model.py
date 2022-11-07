0b7266bc9d5c4acdf38b17ec10ac5a3f00a19389
bakare <bakaredavid007@gmail.com>
2022-11-07 17:38:30 +0100

    test cases

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,58 +1,28 @@
 #!/usr/bin/env python3
 ''' Base Model Testing '''
 import unittest
-import datetime
+from datetime import datetime
 import uuid
 from models.base_model import BaseModel
 
 
-class BaseModelTest(unittest.TestCase):
-    ''' Test Case for BaseModel from models '''
+class TestBaseModel_init(unittest.TestCase):
+    ''' Test Case for BaseModel init '''
 
-    def test_init(self):
-        ''' Init Test'''
+    def test_no_args_init(self):
+        self.assertEqual(BaseModel, type(BaseModel()))
+    
+    def test_id_is_str(self):
+        self.assertIsInstance(BaseModel().id, str)
 
-        lab_rat = BaseModel()
-        self.assertIsInstance(lab_rat, BaseModel)
+    def test_created_at_datetime(self):
+        self.assertIsInstance(BaseModel().created_at, datetime)
 
-    def test_save(self):
-        ''' save() Test'''
+    def test_updated_at_datetime(self):
+        self.assertIsInstance(BaseModel().updated_at, datetime)
+    
+    def test_different_id(self):
+        rat1 = BaseModel()
+        rat2 = BaseModel()
+        self.assertNotEqual(rat1.id, rat2.id)
 
-        lab_rat = BaseModel()
-        old_time = lab_rat.updated_at
-        lab_rat.save()
-        new_time = lab_rat.updated_at
-        self.assertNotEqual(old_time, new_time)
-
-    def test_to_dict(self):
-        ''' to_dict() Test '''
-
-        lab_rat = BaseModel()
-        lab_rat_json = lab_rat.to_dict()
-        self.assertIsInstance(lab_rat_json, dict)
-        for key, values in lab_rat_json.items():
-            self.assertIsInstance(key, str)
-            self.assertIsInstance(values, str)
-
-    def test_self_id(self):
-        ''' self_id Test '''
-
-        lab_rat = BaseModel()
-        lab_rat_len = len(lab_rat.id)
-        new = uuid.uuid4()
-        new_len = len(str(new))
-        self.assertIsInstance(lab_rat.id, str)
-        self.assertEqual(new_len, lab_rat_len)
-
-    def test_self_created_at(self):
-        ''' self_created_at Test '''
-
-        lab_rat = BaseModel()
-        self.assertIsInstance(lab_rat.created_at, datetime.datetime)
-
-    def test__str__(self):
-        ''' String Representation Test '''
-
-        lab_rat = BaseModel()
-        string = lab_rat.__str__
-        self.assertNotIsInstance(string, str)
