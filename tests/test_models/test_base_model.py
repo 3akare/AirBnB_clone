3a9492774bed5e20060c67a488fa0f4448607241
3akare <bakaredavid007@gmail.com>
2022-11-07 20:13:59 +0100

    test cases

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -36,18 +36,8 @@ class TestBaseModel_save(unittest.TestCase):
         sleep(0.05)
         first_updated_at = bm.updated_at
         bm.save()
-        self.assertLess(first_updated_at, bm.updated_at)
-
-    def test_two_saves(self):
-        bm = BaseModel()
-        sleep(0.05)
-        first_updated_at = bm.updated_at
-        bm.save()
-        second_updated_at = bm.updated_at
-        self.assertLess(first_updated_at, second_updated_at)
-        sleep(0.05)
-        bm.save()
-        self.assertLess(second_updated_at, bm.updated_at)
+        self.assertIsInstance(first_updated_at, datetime)
+        self.assertIsInstance(bm.updated_at, str)
 
     def test_save_with_arg(self):
         bm = BaseModel()
