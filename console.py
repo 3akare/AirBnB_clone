e6b1d71485af0ea221bb54591661e44a20605bf5
bakare <bakaredavid007@gmail.com>
2022-11-08 00:29:07 +0100

    added console.py

---
+++ console.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Defines the HBnB console."""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    '''Console Class'''
+
+    prompt = "(hbnb) "
+
+    def do_quit(self, line):
+        '''Exit Program.'''
+        exit()
+
+    def do_EOF(self, line):
+        '''EOF to exit the program.'''
+        return True
+
+    def emptyline(self):
+        '''Doesn't Execute anything'''
+        pass
+
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
